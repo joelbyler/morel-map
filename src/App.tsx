2685d41e9ed5723db36a5f3a1aed754ff86ea3f6
Joel Byler <jbyler@covermymeds.com>
2023-03-10 15:53:17 -0500

    starter app, react, typescript, leaflet, storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './App.css';
+import logo from './logo.svg';
+
 
 function App() {
   return (
@@ -16,9 +18,21 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React X-D
         </a>
       </header>
+
+      <MapContainer style={{ height: 500, width: 500 }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
